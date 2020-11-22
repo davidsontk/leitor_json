dda6c2a745b5ef3eb168488a09b2f6092af54616
Davidson Ferreira <davidson.tk@hotmail.com>
2020-11-22 19:34:38 -0400

    Primeiro commit

---
+++ src/main/java/com/example/leitor_json/LeitorJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.leitor_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitorJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeitorJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leitor_json/LeitorJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leitor_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitorJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
