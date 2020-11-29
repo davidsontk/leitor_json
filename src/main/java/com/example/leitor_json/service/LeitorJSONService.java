37d382fe1214c2216d787f4cd89f02964206b5cc
Davidson Ferreira <davidson.tk@hotmail.com>
2020-11-29 04:28:45 -0400

    Finalizando os ajustes2

--- src/main/java/com/example/leitor_json/service/LeitorJSONService.java
+++ src/main/java/com/example/leitor_json/service/LeitorJSONService.java
@@ -1,11 +1,6 @@
 package com.example.leitor_json.service;
 
-import com.example.leitor_json.dto.BemDTO;
 import com.example.leitor_json.dto.CandidatoDTO;
-import com.example.leitor_json.model.Bem;
-import com.example.leitor_json.model.Eleicao;
-import com.example.leitor_json.repository.BemRepository;
-import com.example.leitor_json.repository.EleicaoRepository;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +8,9 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class LeitorJSONService {
-
-    @Autowired
-    EleicaoRepository eleicaoRepository;
-
-    @Autowired
-    BemRepository bemRepository;
 
     @Autowired
     BancoService bancoService;
@@ -32,16 +19,18 @@ public class LeitorJSONService {
         try {
             File diretorio = new File("./src/main/resources/static");
             if (diretorio.isDirectory()) {
-                File arquivos[] = diretorio.listFiles();
-                for (File arquivo : arquivos) {
-                    CandidatoDTO candidatoDTO = null;
-                    Gson gson = new Gson();
-                    JsonReader reader = new JsonReader(new FileReader(arquivo));
-                    System.out.println("COLOCANDO ARQUIVO COM O NOME = " + arquivo.getName());
-                    candidatoDTO = gson.fromJson(reader, CandidatoDTO.class);
-                    this.popularBanco(candidatoDTO);
+                File[] arquivos = diretorio.listFiles();
+                if(arquivos != null) {
+                    for (File arquivo : arquivos) {
+                        CandidatoDTO candidatoDTO;
+                        Gson gson = new Gson();
+                        JsonReader reader = new JsonReader(new FileReader(arquivo));
+                        System.out.println("COLOCANDO ARQUIVO COM O NOME = " + arquivo.getName());
+                        candidatoDTO = gson.fromJson(reader, CandidatoDTO.class);
+                        this.popularBanco(candidatoDTO);
+                    }
+                    System.out.println("Todos os inserts foram concluídos");
                 }
-                System.out.println("Todos os inserts foram concluídos");
             }
         } catch (Exception e) {
             e.printStackTrace();
